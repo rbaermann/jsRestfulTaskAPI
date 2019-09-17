9a9e683b449677df5db8e65db8ea757a6e86db28
Ryan Baermann <rcbaermann@gmail.com>
2019-09-16 21:30:07 -0700

    added angular

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.static(__dirname + "/public/dist/public"))
 app.use(express.json());
 
 require("./routes")(app);
